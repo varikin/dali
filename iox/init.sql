e656d26a1469087cd5a51ea76963e4a6139414f6
Varikin <varikin@gmail.com>
2008-06-15 22:54:37 -0500

    Updated SQL

--- iox/init.sql
+++ iox/init.sql
@@ -1,32 +1,82 @@
 BEGIN;
-CREATE TABLE "music_playlist" (
+CREATE TABLE "blag_post" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "title" varchar(200) NOT NULL,
+    "content" text NOT NULL,
+    "author" varchar(200) NOT NULL,
+    "created" datetime NOT NULL,
+    "modified" datetime NOT NULL
+)
+;
+CREATE TABLE "blag_media" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "name" varchar(200) NOT NULL,
+    "folder_id" integer NOT NULL
+)
+;
+CREATE TABLE "blag_image" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "name" varchar(200) NOT NULL,
+    "webName" varchar(200) NOT NULL UNIQUE,
+    "original_id" integer NOT NULL REFERENCES "blag_media" ("id"),
+    "viewable_id" integer NOT NULL REFERENCES "blag_media" ("id"),
+    "thumbnail_id" integer NOT NULL REFERENCES "blag_media" ("id"),
+    "description" text NOT NULL,
+    "gallery_id" integer NOT NULL,
+    "order" smallint unsigned NOT NULL
+)
+;
+CREATE TABLE "blag_linklist" (
     "id" integer NOT NULL PRIMARY KEY,
     "name" varchar(200) NOT NULL
 )
 ;
-CREATE TABLE "music_song" (
+CREATE TABLE "blag_tag" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "name" varchar(200) NOT NULL
+)
+;
+CREATE TABLE "blag_link" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "url" varchar(200) NOT NULL,
+    "text" varchar(200) NOT NULL
+)
+;
+CREATE TABLE "blag_folder" (
     "id" integer NOT NULL PRIMARY KEY,
     "name" varchar(200) NOT NULL,
-    "artist" varchar(200) NOT NULL,
-    "album" varchar(200) NOT NULL,
-    "play_count" integer unsigned NULL,
-    "last_played" datetime NULL,
-    "rating" integer unsigned NULL,
-    "filename" varchar(100) NULL
+    "path" varchar(200) NOT NULL,
+    "parent_folder" integer NULL,
+    "webEnabled" bool NOT NULL
 )
 ;
-CREATE TABLE "music_playlist_songs" (
+CREATE TABLE "blag_gallery" (
     "id" integer NOT NULL PRIMARY KEY,
-    "playlist_id" integer NOT NULL REFERENCES "music_playlist" ("id"),
-    "song_id" integer NOT NULL REFERENCES "music_song" ("id"),
-    UNIQUE ("playlist_id", "song_id")
+    "name" varchar(200) NOT NULL,
+    "webName" varchar(200) NOT NULL UNIQUE,
+    "description" text NOT NULL,
+    "parentGallery_id" integer NULL
 )
 ;
-CREATE TABLE "music_song_songs_played_after" (
+CREATE TABLE "blag_post_tags" (
     "id" integer NOT NULL PRIMARY KEY,
-    "from_song_id" integer NOT NULL REFERENCES "music_song" ("id"),
-    "to_song_id" integer NOT NULL REFERENCES "music_song" ("id"),
-    UNIQUE ("from_song_id", "to_song_id")
+    "post_id" integer NOT NULL REFERENCES "blag_post" ("id"),
+    "tag_id" integer NOT NULL REFERENCES "blag_tag" ("id"),
+    UNIQUE ("post_id", "tag_id")
 )
 ;
+CREATE TABLE "blag_linklist_links" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "linklist_id" integer NOT NULL REFERENCES "blag_linklist" ("id"),
+    "link_id" integer NOT NULL REFERENCES "blag_link" ("id"),
+    UNIQUE ("linklist_id", "link_id")
+)
+;
+CREATE INDEX "blag_media_folder_id" ON "blag_media" ("folder_id");
+CREATE INDEX "blag_image_original_id" ON "blag_image" ("original_id");
+CREATE INDEX "blag_image_viewable_id" ON "blag_image" ("viewable_id");
+CREATE INDEX "blag_image_thumbnail_id" ON "blag_image" ("thumbnail_id");
+CREATE INDEX "blag_image_gallery_id" ON "blag_image" ("gallery_id");
+CREATE INDEX "blag_folder_parent_folder" ON "blag_folder" ("parent_folder");
+CREATE INDEX "blag_gallery_parentGallery_id" ON "blag_gallery" ("parentGallery_id");
 COMMIT;
