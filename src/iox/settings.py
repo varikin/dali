e2a9020e879fccdbdc1469212b9c195af0d2f99c
Varikin <varikin@gmail.com>
2008-06-22 01:04:08 -0500

    Renamed admin.html to base.html

--- src/iox/settings.py
+++ src/iox/settings.py
@@ -10,7 +10,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/home/varikin/code/blag/src/iox/iox.db'             # Or path to database file if using sqlite3.
+DATABASE_NAME = '/home/varikin/code/blag/code/src/iox/iox.db'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -70,8 +70,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/varikin/code/blag/src/templates/admin",
-    "/home/varikin/code/blag/src/templates",
+    "/home/varikin/code/blag/code/src/templates/admin",
+    "/home/varikin/code/blag/code/src/templates",
 )
 
 INSTALLED_APPS = (
