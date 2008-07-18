8bfc89e6ec67da1d47322896c9b6b48b1ca42973
varikin <varikin@overdrive.local>
2008-07-17 23:06:49 -0500

    Removed all the Folder/Media stuff and just using a regular ImageField now for the images.  Some work still needs to be done on this, but it is close.
    Also removed all the blog related models in models.py.  This is now just a gallery app.

--- blag/models.py
+++ blag/models.py
@@ -1,70 +1,15 @@
 from django.db import models
 import os
 
-# Create your models here.
-class Folder(models.Model):
-	name = models.CharField(max_length = 200)
-	#folder path is relative to the parent.  If root, absolute
-	path = models.CharField(max_length = 200)
-	parentFolder = models.ForeignKey('self', db_column = 'parent_folder', null=True)
-	webEnabled = models.BooleanField(default = False)
-	
-	def __unicode__(self):
-		return self.name
-
-	def getPath(self):
-		current = self
-		path = current.path
-		while not current.isRoot():
-			current = current.parentFolder
-			path = current.path + os.sep + path
-		return path
-		
-	def getUrl(self):
-		current = self
-		webSep = "/"
-		path = current.path
-		while not current.isRoot():
-			current = current.parentFolder
-			if not current.webEnabled:
-				break
-			path = current.path + webSep + path
-		return path
-		
-	def isRoot(self):
-		result = False
-		if self.parentFolder is None:
-			result = True
-		return result
-		
-		
-class Media(models.Model):
-	name = models.CharField(max_length = 200)
-	folder = models.ForeignKey(Folder)
-	
-	def __unicode__(self):
-		return self.name
-		
-	def getPath(self):
-		return self.folder.getPath() + os.sep + self.name
-		
-	def getUrl(self):
-		webSep = "/"
-		return webSep + self.folder.getUrl() + webSep + self.name
-		
-	def saveFile(self, image):
-		image.save( self.getPath(), "PNG")
-
-		
 class Gallery(models.Model):
 	name = models.CharField(max_length = 200)
-	webName = models.CharField(max_length=200, unique = True)
+	webName = models.CharField(max_length = 200, unique = True)
 	description = models.TextField()
-	parentGallery = models.ForeignKey('self', null=True)
+	parentGallery = models.ForeignKey('self', null = True, blank = True)
 	
 	def __unicode__(self):
 		return self.name
-		
+	
 	def getRandomImage(self):
 	    try:
 	        image = Image.objects.filter(gallery = self).order_by('?')[0:1].get()
@@ -74,16 +19,21 @@ class Gallery(models.Model):
 	    return image
 	
 	def getImageCount(self):
-	    ''' Returns the number of images in the gallery; return an integer '''
+	    """
+	    Returns the number of images in the gallery; return an integer
+	    """
 	    count = Image.objects.filter(gallery = self).count()
 	    return count
-	    
+	
+	class Admin:
+	    pass
+
 class Image(models.Model):
 	name = models.CharField(max_length = 200)
 	webName = models.CharField(max_length = 200, unique = True)
-	original = models.ForeignKey(Media, related_name = "original")
-	viewable = models.ForeignKey(Media, related_name = "viewable")
-	thumbnail = models.ForeignKey(Media, related_name = "thumbnail")
+	original = models.ImageField(upload_to = "original")
+	viewable = models.ImageField(upload_to = "viewable")
+	thumbnail = models.ImageField(upload_to = "thumbnail")
 	description = models.TextField()
 	gallery = models.ForeignKey(Gallery)
 	order = models.PositiveSmallIntegerField()
@@ -91,38 +41,5 @@ class Image(models.Model):
 	def __unicode__(self):
 		return self.name
 	
-	
-class Link(models.Model):
-	url = models.URLField(max_length = 200, verify_exists = False)
-	text = models.CharField(max_length = 200)
-	
-	def __unicode__(self):
-		return self.text
-	
-	
-class LinkList(models.Model):
-	name = models.CharField(max_length = 200)
-	links = models.ManyToManyField(Link)
-	
-	def __unicode__(self):
-		return self.name
-	
-	
-class Tag(models.Model):
-	name = models.CharField(max_length = 200)
-	
-	def __unicode__(self):
-		return self.name
-	
-	
-class Post(models.Model):
-	title = models.CharField(max_length = 200)
-	content = models.TextField()
-	tags = models.ManyToManyField(Tag)
-	author = models.CharField(max_length = 200)
-	created = models.DateTimeField(auto_now_add = True)
-	modified = models.DateTimeField(auto_now = True)
-	
-	def __unicode__(self):
-		return self.title
-	
+	class Admin:
+	    pass
--- blag/views.py
+++ blag/views.py
@@ -1,44 +1,7 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.http import Http404
-from blag.models import Image, Media, Gallery, Folder
-from blag import handlers
-        
-def createImage(request):
-	if request.method == 'POST':
-		requiredValues = hasRequiredValues(request.POST, \
-			[ 'name', 'description', 'webName', 'gallery' ])
-		validImage = hasValidImage(request.FILES, 'imageFile')  
-		if requiredValues and validImage:
-			name = request.POST['name']
-			description = request.POST['description']
-			webName = request.POST['webName']
-			gallery = request.POST['gallery']
-			imageFile = request.FILES['imageFile']
-
-			image = handlers.createImage(name, webName, description, gallery, imageFile)
-
-
-	galleries = Gallery.objects.all()
-	return render_to_response('blag/createImage.html', {'galleries': galleries})
-	
-
-def createGallery(request):
-	if request.method == 'POST':
-		requiredValues = ['name', 'description', 'webName']
-		if hasRequiredValues(request.POST, requiredValues): 
-			name = request.POST['name']
-			description = request.POST['description']
-			webName = request.POST['webName']
-			parent = request.POST.get('parent', None)
-
-			gallery = handlers.createGallery(name, webName, description, parent)
-
-	
-	galleries = Gallery.objects.all()
-
-	return render_to_response('blag/createGallery.html', {'galleries': galleries})
-
+from blag.models import Image, Gallery
 
 def gallery_detail(request, webName):
 
@@ -49,19 +12,3 @@ def gallery_detail(request, webName):
 		raise Http404
 	
 	return render_to_response('blag/galleryDetail.html', {'gallery': gallery, 'images': images})
-
-def hasRequiredValues(post, requiredValues):
-	result = True
-	for requiredValue in requiredValues:
-		if not post.has_key(requiredValue):
-			result = False
-
-	return result
-
-def hasValidImage(files, imageName):
-	result = False
-	if files.has_key(imageName):
-		if files[imageName]['content-type'].startswith('image'):
-			result = True
-
-	return result
