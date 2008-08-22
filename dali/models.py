de2de28e7e0b663c2da600b490e2e012a37bb938
varikin <varikin@overdrive.local>
2008-08-21 23:12:56 -0500

    Added DnD sort to the picture change list. Added generate_images preference. Removed old unused templates. Moved templates to within Dali folder. Removed jQuery.ui demos, tests, i18n, packed, and minified files.  Not needed for development.

--- dali/models.py
+++ dali/models.py
@@ -13,29 +13,29 @@ class Gallery(models.Model):
     published = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
-        
+    
     def __unicode__(self):
         return self.name
     
     def getRandomPicture(self):
         """
-        Return a random picture from the gallery.  If not pictures are in the gallery, 
+        Return a random picture from the gallery.  If no pictures are in the gallery, 
         None is returned.
         """
         try:
-            picture = Picture.objects.filter(gallery = self).order_by('?')[0:1].get()
+            picture = Picture.objects.filter(gallery=self).order_by('?')[0:1].get()
         except Picture.DoesNotExist:
             picture = None
-        
         return picture
     
     def getPictureCount(self):
         """
         Return the number of pictures in the gallery; return an integer.
         """
-        return Picture.objects.filter(gallery = self).count()
-        
+        return Picture.objects.filter(gallery=self).count()
+
     getPictureCount.short_description = 'Number of Pictures'
+        
 
 class Picture(models.Model):
     name = models.CharField(max_length=200)
@@ -58,29 +58,29 @@ class Picture(models.Model):
         
         Generates a thumbnail and viewable from the original image using PIL.
         """
-        orig = Image.open(self.original.path)
-        name = os.path.basename(self.original.name)
-        
         prefs = Preferences.objects.all()[0:1].get()
-        thumb_width = prefs.thumbnail_width
-        view_width = prefs.viewable_width
-       
-	
-        thumb_temp = _get_resized_image(orig, thumb_width)
-	self.thumbnail.save(name, File(thumb_temp), False)
+        if(self.id is not None and prefs.generate_images):
+            thumb_width = prefs.thumbnail_width
+            view_width = prefs.viewable_width
+            orig = Image.open(self.original.path)
+            name = os.path.basename(self.original.name)
+            
+            thumb_temp = _get_resized_image(orig, thumb_width)
+    	    self.thumbnail.save(name, File(thumb_temp), False)
+    	    
+    	    view_temp = _get_resized_image(orig, view_width)
+            self.viewable.save(name, File(view_temp), False)
+            
+            super(Picture, self).save()
+            thumb_temp.close()
+            view_temp.close()
+        else:
+            super(Picture, self).save()
         
-	view_temp = _get_resized_image(orig, view_width)
-        self.viewable.save(name, File(view_temp), False)
-        
-        super(Picture, self).save()
-        
-        thumb_temp.close()
-        view_temp.close()
-        
-
 class Preferences(models.Model):
     thumbnail_width = models.PositiveSmallIntegerField()
     viewable_width = models.PositiveSmallIntegerField()
+    generate_images = models.BooleanField(default=False)
     
     def __unicode__(self):
         return u'Preferences'
@@ -92,8 +92,6 @@ class Preferences(models.Model):
         """
         if(self.id is not None or Picture.objects.count() == 0):
             super(Preferences, self).save()
-
-
 
 def _get_resized_image(image, width):
     """
@@ -107,7 +105,6 @@ def _get_resized_image(image, width):
     Parameters: 
     image: A PIL Image object
     size: The width in pixels to resize the image to.  
-    
     """
     height = int(width * image.size[1] / image.size[0])
     resized = image.resize((width, height), Image.ANTIALIAS)
