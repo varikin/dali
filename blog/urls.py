85f6c379e856eff4d0f3ff33f875add02454e1c6
varikin <varikin@overdrive.local>
2009-01-11 21:47:19 -0600

    Removed date based urls for blog posts.
    Also added privileged post querysets to allow authenticated users access to
    unpublished posts.  Followed the pattern like http://github.com/brosner/oebfare/tree/master.

--- blog/urls.py
+++ blog/urls.py
@@ -1,31 +1,13 @@
 from django.conf.urls.defaults import *
 from blog.models import Post
+from blog.views import post_list, post_detail, update_post
 
-date_based = {
-    'date_field': 'date_published',
-    'queryset': Post.objects.published(),
-}
-latest = dict(date_based)
-archive = dict(date_based, **{'template_object_name': 'post'})
-
-urlpatterns = patterns('django.views.generic.date_based',
-    url(r'^$', 'archive_index', latest, name='blog_post_index'),
-    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', 
-        'archive_day', archive, name='blog_archive_day'),
-    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', 
-        'archive_month', archive, name='blog_archive_month'),
-    url(r'^(?P<year>\d{4})/$', 
-        'archive_year', archive, name='blog_archive_year'),
-)
-
-
-post_detail = {
-    'queryset': Post.objects.all(),
-    'template_object_name': 'post',  
+post_dict = {
+    'template_object_name': 'post' 
 }
 
-urlpatterns += patterns('',
-    url(r'^(?P<slug>[-\w]+)/update/$', 'blog.views.update_post', name='blog_update_post'),
-    url(r'^(?P<slug>[-\w]+)/$', 'django.views.generic.list_detail.object_detail', 
-        post_detail, name='blog_post_detail'),
+urlpatterns = patterns('',
+    url(r'^(?P<slug>[-\w]+)/update/$', update_post, name='blog_update_post'),
+    url(r'^(?P<slug>[-\w]+)/$', post_detail, post_dict, name='blog_post_detail'),
+    url(r'^$', post_list, post_dict, name='blog_post_index'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,33 @@
 import logging
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse
+from django.views.generic import list_detail
 from blog.models import Post
+
+def privileged_post_queryset(view):
+    """
+    Decorator to set the Post queryset.
+    
+    If the user is authenticated, return all posts, else only the published posts.
+    """
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_authenticated():
+            kwargs['queryset'] = Post.objects.all().order_by('-date_published')
+        else:
+            kwargs['queryset'] = Post.objects.published().order_by('-date_published')
+        return view(request, *args, **kwargs)
+    return _wrapped_view
+
+post_list = privileged_post_queryset(list_detail.object_list)
+post_detail = privileged_post_queryset(list_detail.object_detail)
 
 @permission_required('blog.change_post')
 def update_post(request, slug):
-    """Updates a post title. Permission required"""
+    """
+    Updates a post for the given slug.
+    
+    For every key in the POST, tries to set a post attr with the key name.
+    """
     status = u'Failure'
     if request.method == 'POST':
         try:
@@ -18,7 +40,7 @@ def update_post(request, slug):
                     setattr(post, key, value)
                     updated = True
                 else:
-                    logging.warn("Error, %s part of Post" % key)
+                    logging.warn("Warning, %s isn't part of Post" % key)
             
             if updated:
                 post.save()
@@ -26,6 +48,5 @@ def update_post(request, slug):
         except (KeyError, Post.DoesNotExist):
             logging.error("Error, no post with slug: %s" % slug)
     else:
-        loggiing.error("Error, not a HTTP POST")
-        
+        loggiing.error("Error, not a HTTP POST")     
     return HttpResponse(status, mimetype="text/plain")
