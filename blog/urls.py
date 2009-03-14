b53a6345125dec992350c8bb47125415dc4490e5
varikin <varikin@overdrive.local>
2009-03-14 14:45:32 -0500

    Added a latest feed to the blog.

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,20 @@
 from django.conf.urls.defaults import *
+from django.contrib.syndication.views import feed
+from blog.feeds import LatestPosts
 from blog.models import Post
 from blog.views import post_list, post_detail, update_post
 
-post_dict = {
+posts = {
     'template_object_name': 'post' 
 }
 
+feeds = {
+    'latest': LatestPosts,
+}
+
 urlpatterns = patterns('',
+    url(r'^feeds/(?P<url>.*)/$', feed, {'feed_dict': feeds}),
     url(r'^(?P<slug>[-\w]+)/update/$', update_post, name='blog_update_post'),
-    url(r'^(?P<slug>[-\w]+)/$', post_detail, post_dict, name='blog_post_detail'),
-    url(r'^$', post_list, post_dict, name='blog_post_index'),
+    url(r'^(?P<slug>[-\w]+)/$', post_detail, posts, name='blog_post_detail'),
+    url(r'^$', post_list, posts, name='blog_post_index'),
 )
