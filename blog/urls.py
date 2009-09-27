dcb728b450daee58cd7f76ceff9dbe4277e74f9a
John Shimek <varikin@gmail.com>
2009-09-27 18:46:41 -0500

    Added pagination to the blog post listing.

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,6 @@ from blog.feeds import LatestPosts
 from blog.models import Post
 from blog.views import post_list, post_detail, update_post
 
-posts = {
-    'template_object_name': 'post' 
-}
-
 feeds = {
     'latest': LatestPosts,
 }
@@ -15,6 +11,11 @@ feeds = {
 urlpatterns = patterns('',
     url(r'^feeds/(?P<url>.*)/$', feed, {'feed_dict': feeds}, name='blog_feed'),
     url(r'^(?P<slug>[-\w]+)/update/$', update_post, name='blog_update_post'),
-    url(r'^(?P<slug>[-\w]+)/$', post_detail, posts, name='blog_post_detail'),
-    url(r'^$', post_list, posts, name='blog_post_index'),
+    url(r'^(?P<slug>[-\w]+)/$', post_detail, {
+        'template_object_name': 'post',
+    }, name='blog_post_detail'),
+    url(r'^$', post_list, {
+        'template_object_name': 'post',
+        'paginate_by': 10,
+    }, name='blog_post_index'),
 )
