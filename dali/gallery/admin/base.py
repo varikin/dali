dae95ffeb21d4a990d0c42a9b8d406e0ea90e3cd
John Shimek <varikin@gmail.com>
2009-12-09 21:58:56 -0600

    Use the CKEditor on the picture admin.

--- dali/gallery/admin/base.py
+++ dali/gallery/admin/base.py
@@ -10,6 +10,12 @@ class GalleryAdmin(admin.ModelAdmin):
     search_fields = ('name', 'slug', 'parent_gallery__name')
     prepopulated_fields = {'slug': ('name',)}
 
+    class Media:
+	    js = (
+	        '/static/js/ckeditor/ckeditor.js',
+	        '/static/js/ckeditor_init.js'
+	    )
+
 class PictureAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'gallery', 'order', 
         'date_created', 'date_modified')
@@ -19,6 +25,12 @@ class PictureAdmin(admin.ModelAdmin):
     list_filter = ('gallery', 'date_created', 'date_modified')
     fields = ('name', 'slug', 'description', 'original', 'gallery', 'order')
     prepopulated_fields = {'slug': ('name',)}
+
+    class Media:
+	    js = (
+	        '/static/js/ckeditor/ckeditor.js',
+	        '/static/js/ckeditor_init.js'
+	    )
         
 admin.site.register(Gallery, GalleryAdmin)
 admin.site.register(Picture, PictureAdmin)
