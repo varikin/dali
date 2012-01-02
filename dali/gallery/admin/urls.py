deb9b81837efaa07f4ecd8bdd757610da8bddd59
John Shimek <varikin@gmail.com>
2012-01-02 16:40:37 -0600

    Same as last. sigh

--- dali/gallery/admin/urls.py
+++ dali/gallery/admin/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 #gallery.views views
-urlpatterns = patterns('gallery.admin.views',
+urlpatterns = patterns('dali.gallery.admin.views',
     url(r'^picture/save_order/$', 'save_picture_order', name='save_picture_order'),
     url(r'^picture/add_from_zip/$', 'add_pictures_from_zip', name='add_pictures_from_zip'),
     url(r'^gallery/save_order/$', 'save_gallery_order', name='save_gallery_order'),
